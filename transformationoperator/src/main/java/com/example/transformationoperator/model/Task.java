f75628b7c8c07c970554161c96a4792980153567
Frolix <kostya.lotysh@gmail.com>
2019-10-23 14:17:51 +0300

    modified transformation operator module

--- transformationoperator/src/main/java/com/example/transformationoperator/model/Task.java
+++ transformationoperator/src/main/java/com/example/transformationoperator/model/Task.java
@@ -1,4 +1,37 @@
 package com.example.transformationoperator.model;
 
 public class Task {
+    private String description;
+    private boolean isComplete;
+    private int preority;
+
+    public Task(String description, boolean isComplete, int preority) {
+        this.description = description;
+        this.isComplete = isComplete;
+        this.preority = preority;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(boolean complete) {
+        isComplete = complete;
+    }
+
+    public int getPreority() {
+        return preority;
+    }
+
+    public void setPreority(int preority) {
+        this.preority = preority;
+    }
 }
--- transformationoperator/src/main/java/com/example/transformationoperator/view/MainActivity.java
+++ transformationoperator/src/main/java/com/example/transformationoperator/view/MainActivity.java
@@ -1,14 +1,257 @@
-package com.example.transformationoperator;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.transformationoperator.view;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import com.example.transformationoperator.R;
+import com.jakewharton.rxbinding3.view.RxView;
+
+import java.util.concurrent.TimeUnit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import kotlin.Unit;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private long timeSinceLastRequest;
+
+    private Button button;
+
+
+    CompositeDisposable disposable = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.rx_btn);
+
+        RxView.clicks(button)
+                .throttleFirst(500, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Unit>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposable.add(d);
+                    }
+
+                    @Override
+                    public void onNext(Unit unit) {
+                        Log.d(TAG, "onNext: time since last clicked : "+(System.currentTimeMillis()-timeSinceLastRequest));
+                        someMethod();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+
+
+
+
+
+
+    }
+
+    private void someMethod() {
+        timeSinceLastRequest = System.currentTimeMillis();
+        //do something
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposable.clear();
     }
 }
+
+/*
+
+
+    private void sendRequestToServer(String s) {
+        //do nothing
+    }
+
+
+        Observable<String> observableQueryText = Observable
+                .create(new ObservableOnSubscribe<String>() {
+                    @Override
+                    public void subscribe(final ObservableEmitter<String> emitter) throws Exception {
+                        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                            @Override
+                            public boolean onQueryTextSubmit(String query) {
+                                return false;
+                            }
+
+                            @Override
+                            public boolean onQueryTextChange(String newText) {
+                                if (!emitter.isDisposed()){
+                                    emitter.onNext(newText);
+                                }
+                                return false;
+                            }
+                        });
+                    }
+                })
+                .debounce(100, TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.io());
+
+
+
+        observableQueryText.subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                disposable.add(d);
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "onNext: time since last request: "+(System.currentTimeMillis()-timeSinceLastRequest));
+                Log.d(TAG, "onNext: search query: "+s);
+                timeSinceLastRequest = System.currentTimeMillis();
+
+                //method for sending a request to the server
+                sendRequestToServer(s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+
+
+
+
+
+        RxView.clicks(findViewById(R.id.rx_btn))
+                .map(new Function<Unit, Integer>() {
+                    @Override
+                    public Integer apply(Unit unit) throws Exception{return 1;}
+                })
+                .buffer(4, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<List<Integer>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposable.add(d);
+                    }
+
+                    @Override
+                    public void onNext(List<Integer>integers) {
+                        Log.d(TAG, "onNext: you clicked "+integers.size()+"times in 4 seconds");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposable.clear();
+    }
+
+    Observable<Task> taskObservable = Observable
+                .fromIterable(DataSource.createTasksList())
+                .subscribeOn(Schedulers.io());
+
+        taskObservable
+                .buffer(2)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<List<Task>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(List<Task> tasks) {
+                        Log.d(TAG, "onNext: bundle result: -------");
+                        for (Task task:tasks){
+                            Log.d(TAG, "onNext: "+task.getDescription());
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+
+   Function<Task,String> extractDescriptionFunction = new Function<Task, String>() {
+        @Override
+        public String apply(Task input) {
+            Log.d(TAG, "apply: doing work on thread : "+Thread.currentThread().getName());
+            return input.getDescription();
+        }
+    };
+
+
+*  Observable<String> extractDescriptionObservable = Observable
+                .fromIterable(DataSource.createTasksList())
+                .subscribeOn(Schedulers.io())
+                .map(new io.reactivex.functions.Function<Task, String>() {
+                    @Override
+                    public String apply(Task task) throws Exception {
+                        Log.d(TAG, "apply: doing work on thread : "+Thread.currentThread().getName());
+                        task.setComplete(true);
+                        return task.getDescription();
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread());
+
+        extractDescriptionObservable.subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+            }
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "onNext: extracted description: " + s);
+            }
+            @Override
+            public void onError(Throwable e) {
+            }
+            @Override
+            public void onComplete() {
+            }
+        });
+
+* */
